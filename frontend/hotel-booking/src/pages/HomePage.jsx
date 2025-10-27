ae5aaaca13fbea84370cefe111a3a2783630fea4
H82M19\Student <Student>
2025-10-27 09:36:15 +0700

    Done basic Homepage

---
+++ frontend/hotel-booking/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/homepage/Header";
+import HeroSearch from "../../components/homepage/HeroSearch";
+import DestinationList from "../../components/homepage/DestinationList";
+import Footer from "../../components/homepage/Footer";
+export default function HomePage() {
+  
+  return (
+    <>
+      <Header />
+      <HeroSearch />
+      <DestinationList/>
+      <Footer />
+    </>
+  );
+}
