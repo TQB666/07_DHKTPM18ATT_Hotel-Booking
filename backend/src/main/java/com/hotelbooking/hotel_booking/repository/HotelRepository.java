065943e1b34cbe2726ff75960f5ac2354584e369
H82M19\Student <Student>
2025-10-27 09:32:40 +0700

    add Hotel repository

---
+++ backend/src/main/java/com/hotelbooking/hotel_booking/repository/HotelRepository.java
@@ -0,0 +1,18 @@
+package com.hotelbooking.hotel_booking.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import com.hotelbooking.hotel_booking.domain.Hotel;
+
+public interface HotelRepository extends JpaRepository<Hotel,Long>, JpaSpecificationExecutor<Hotel>{
+    List<Hotel> findAll();
+    // Đếm số hotel theo city, trả về list object
+    @Query("SELECT h.city AS city, COUNT(h) AS totalHotels FROM Hotel h GROUP BY h.city")
+    List<Object[]> countHotelsByCity();
+
+    List<Hotel> findByCity(String city);
+}
