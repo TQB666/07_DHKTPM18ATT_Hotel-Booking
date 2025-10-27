6cd85242e963e0d505a4a47d182d6abb5ebda248
Liem <liem@gmail.com>
2025-10-27 09:51:47 +0700

    .

--- frontend/hotel-booking/src/components/homepage/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-4 mt-8">
-      <p>© 2025 HotelBooking. All rights reserved.</p>
-    </footer>
-  );
-}
