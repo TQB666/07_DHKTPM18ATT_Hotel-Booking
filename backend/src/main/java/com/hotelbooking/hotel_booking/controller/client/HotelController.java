e87ee7103936d38423356eea8f59ff61e2a4c7a8
TQB666 <baotz147@gmail.com>
2025-10-27 09:38:00 +0700

    add Hotel Controller

---
+++ backend/src/main/java/com/hotelbooking/hotel_booking/controller/client/HotelController.java
@@ -0,0 +1,42 @@
+package com.hotelbooking.hotel_booking.controller.client;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hotelbooking.hotel_booking.domain.Hotel;
+import com.hotelbooking.hotel_booking.service.HotelService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/hotels")
+public class HotelController {
+    private final HotelService hotelService;
+
+    // Lấy danh sách khách sạn, có thể lọc theo nếu client truyền query param
+    @GetMapping("/filter")
+    public List<Hotel> getHotels(
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer stars,
+            @RequestParam(required = false) Double minPrice,
+            @RequestParam(required = false) Double maxPrice) {
+        return hotelService.searchHotels(city, name, stars, minPrice, maxPrice);
+        }
+    @GetMapping("/{id}")
+    public Hotel getHotelById(@PathVariable Long id) {
+        return hotelService.getHotelById(id);
+    }
+
+
+    
+        
+
+    
+}
